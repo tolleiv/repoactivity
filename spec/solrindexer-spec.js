4c63718cde1959b37c5748e7341c2498903679e9
Tolleiv Nietsch <info@tolleiv.de>
2014-03-03 22:33:23 +0100

    Travis adjustments pt2

--- spec/solrindexer-spec.js
+++ spec/solrindexer-spec.js
@@ -84,7 +84,7 @@ describe("The Solr indexer", function () {
             solr.findRepositories(function (err, docs) {
                 expect(err).toBeNull();
                 expect(docs.length).toBe(2);
-                expect(docs[0].type).toBe('repository')
+                expect(docs['0'].type).toBe('repository')
                 done();
             });
         });
