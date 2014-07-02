92afbcf78b86080cb5dbd9335a0d947e5f48eec1
Tolleiv Nietsch <info@tolleiv.de>
2014-07-03 00:21:18 +0200

    Express 4.x upgrade and jade inclusion

--- routes/main.js
+++ routes/main.js
@@ -2,13 +2,13 @@ var cluster = require('cluster');
 
 module.exports = function (app) {
     app.get("/", function (req, res) {
-        res.send(200);
+        res.render('index', { title: 'Home' })
     });
 
-    app.get("/app-status", function(req, res) {
+    app.get("/app-status", function (req, res) {
         var solr = require('../lib/solr')(process.env.NODE_ENV || 'development')
         solr.ping(function (err, doc) {
-            var status = ( err ? ":( " : ":) ")  + (process.env.NODE_ENV || 'development');
+            var status = ( err ? ":( " : ":) ") + (process.env.NODE_ENV || 'development');
             res.send(err ? 500 : 200, status);
         });
     });
